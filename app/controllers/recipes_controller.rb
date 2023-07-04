19a8dbbe0a6d104223d6e22cb57d027ef8ac327f
knskymsk <knsk.smnc.30@gmail.com>
2023-07-04 18:16:54 +0900

    Update recipes_controller.rb
    修正

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -14,6 +14,7 @@ class RecipesController < ApplicationController
     if @recipe.save
       redirect_to root_path, notice: "投稿が成功しました。"
     else
+      puts @recipe.errors.full_messages  # エラーメッセージを出力
       render :new
     end
   end
