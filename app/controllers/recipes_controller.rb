37fd6ee81e594a54c784e9381a37f4837a2f4cf6
knskymsk <knsk.smnc.30@gmail.com>
2023-07-11 12:06:25 +0900

    修正

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,7 +10,8 @@ class RecipesController < ApplicationController
 
   def create
     @recipe = Recipe.new(recipe_params)
-    @recipe.user = current_user
+    # @recipe.user = current_user
+    @recipe.user
     # binding.pry
     if @recipe.save
       redirect_to recipes_path
