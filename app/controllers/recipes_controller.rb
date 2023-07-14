424f4684438f863b46431cea3db348efbf6f05ba
knskymsk <knsk.smnc.30@gmail.com>
2023-07-14 10:43:07 +0900

    修正

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -11,9 +11,9 @@ class RecipesController < ApplicationController
   def create
     @recipe = Recipe.new(recipe_params)
     @recipe.user = current_user
-    binding.pry
+    # binding.pry
     if @recipe.save
-      redirect_to root_path # トップページにリダイレクト
+      redirect_to root_path
     else
       render :new
     end
